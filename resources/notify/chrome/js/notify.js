239e4de4fcb96ff9ef12c9e1e5f0b6dc48ac33c7
kuaifan <aipaw@live.cn>
2020-06-08 17:47:41 +0800

    新增谷歌浏览器插件

---
+++ resources/notify/chrome/js/notify.js
@@ -0,0 +1,132 @@
+﻿/**
+ * 连接已有会员
+ * @type {*}
+ */
+var tagId = 0;
+const instances = {};
+const updateBadgeNum = function () {
+    var badgeNum = 0;
+    for (var index in instances) {
+        if (!instances.hasOwnProperty(index)) {
+            continue;
+        }
+        badgeNum+= instances[index].unread;
+    }
+    if (badgeNum == 0) {
+        chrome.browserAction.setBadgeText({text: ''});
+    } else {
+        chrome.browserAction.setBadgeText({text: (badgeNum > 99 ? '99+' : badgeNum) + ''});
+    }
+    chrome.runtime.sendMessage({
+        act: 'instances',
+        instances: instances
+    });
+}
+const getBadgeNum = function () {
+    var configLists = $A.jsonParse($A.getStorage("configLists"), {});
+    for (var index in configLists) {
+        if (!configLists.hasOwnProperty(index)) {
+            continue;
+        }
+        const key = index;
+        const config = configLists[key];
+        if (typeof instances[key] === "undefined") {
+            instances[key] = {};
+        }
+        //
+        if (instances[key].username !== config.username) {
+            instances[key].username = config.username;
+            instances[key].token = config.token;
+            instances[key].unread = 0;
+            instances[key].open = false;
+            updateBadgeNum();
+            if (typeof instances[key].ws !== "undefined") {
+                instances[key].ws.config(null).close();
+            }
+            instances[key].ws = new WTWS({
+                username: config.username,
+                token: config.token,
+                url: config.url,
+                channel: 'chromeExtend'
+            }).setOnMsgListener('notify', ['open', 'unread', 'user'], function (msgDetail) {
+                let body = msgDetail.body;
+                if (['taskA'].indexOf(body.type) !== -1) {
+                    return;
+                }
+                switch (msgDetail.messageType) {
+                    case 'open':
+                        instances[key].open = true;
+                        break;
+                    case 'unread':
+                        instances[key].unread = msgDetail.body.unread;
+                        break;
+                    case 'user':
+                        instances[key].unread++;
+                        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+                            if ($A.getHostname(tabs[0].url) != key) {
+                                var url = 'http://' + key + '/#/todo?token=' + encodeURIComponent(instances[key].token) + '&open=chat';
+                                $A.showNotify(key, {
+                                    body: $A.getMsgDesc(body),
+                                    icon: body.userimg
+                                }, url);
+                            }
+                        });
+                        break;
+                }
+                updateBadgeNum();
+            }).sendTo('unread', function (res) {
+                if (res.status === 1) {
+                    instances[key].unread = $A.runNum(res.message);
+                    updateBadgeNum();
+                }
+            });
+        }
+    }
+    //
+    tagId++;
+    const tmpID = tagId;
+    setTimeout(function () {
+        if (tmpID === tagId) {
+            getBadgeNum();
+        }
+    }, 5000);
+}
+getBadgeNum();
+
+/**
+ * 监听来自网站的会员信息
+ */
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    var configLists;
+    if (request.act === "config") {
+        if (sender.tab) {
+            var hostname = $A.getHostname(sender.tab.url);
+            if (hostname) {
+                configLists = $A.jsonParse($A.getStorage("configLists"), {});
+                if (typeof configLists[hostname] !== "object") {
+                    configLists[hostname] = {};
+                }
+                configLists[hostname] = Object.assign(request.config, {
+                    hostname: hostname,
+                });
+                $A.setStorage("configLists", $A.jsonStringify(configLists));
+                sendResponse(configLists);
+            }
+        }
+    } else if (request.act === "getInstances") {
+        sendResponse(instances);
+    } else if (request.act === "delInstances") {
+        configLists = $A.jsonParse($A.getStorage("configLists"), {});
+        if (typeof configLists[request.index] === "object") {
+            delete configLists[request.index];
+            $A.setStorage("configLists", $A.jsonStringify(configLists));
+        }
+        if (typeof instances[request.index] === "object") {
+            if (typeof  instances[request.index].ws !== "undefined") {
+                 instances[request.index].ws.config(null).close();
+            }
+            delete instances[request.index];
+        }
+        updateBadgeNum();
+    }
+});
---
+++ resources/notify/chrome/js/popup.js
@@ -0,0 +1,65 @@
+﻿chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.act === "instances") {
+        showLists(request.instances);
+    }
+});
+
+chrome.runtime.sendMessage({
+    act: 'getInstances'
+}, function (response) {
+    showLists(response);
+});
+
+function onDelete(index) {
+    chrome.runtime.sendMessage({
+        act: 'delInstances',
+        index: index,
+    });
+}
+
+function showLists(lists) {
+    var html = '';
+    console.log(lists);
+    for (var index in lists) {
+        if (!lists.hasOwnProperty(index)) {
+            continue;
+        }
+        const item = lists[index];
+        html+= '<li class="message_box" data-index="' + index + '" data-token="' + item.token + '">';
+        html+= '<div class="message_username">' + item.username + '</div>';
+        html+= '<div class="message_host">' + index + '</div>';
+        html+= '<div class="message_unread">未读: ' + item.unread + '</div>';
+        html+= '<div class="message_delete">删除</div>';
+        html+= '</li>';
+    }
+    if (!html) {
+        html+= '<li class="message_box">';
+        html+= '<div class="message_loading">没有相关的记录！</div>';
+        html+= '</li>';
+    }
+    $("#message_div").html('<ul>' + html + '</ul>');
+    $("div.message_delete").click(function(){
+        if (confirm("确定要删除此记录吗？")) {
+            onDelete($(this).parents("li").attr("data-index"));
+        }
+    });
+    $("div.message_unread,div.message_host").click(function(){
+        const index = $(this).parents("li").attr("data-index");
+        const token = encodeURIComponent($(this).parents("li").attr("data-token"));
+        chrome.tabs.query({}, function (tabs) {
+            var has = false;
+            tabs.some(function (item) {
+                if ($A.getHostname(item.url) == index) {
+                    chrome.windows.update(item.windowId, {focused: true});
+                    chrome.tabs.highlight({tabs: item.index, windowId: item.windowId});
+                    chrome.tabs.update({url: $A.urlAddParams($A.removeURLParameter(item.url, ['open', 'rand']), {open: 'chat', rand: Math.round(new Date().getTime())})});
+                    return has = true;
+                }
+            });
+            if (!has) {
+                chrome.tabs.create({ url: 'http://' + index + '/#/todo?token=' + token + '&open=chat' })
+            }
+        });
+    })
+}
+
