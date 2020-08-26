2cd5c351198f86386551bb32849c8ab302bf47cb
kuaifan <aipaw@live.cn>
2020-08-27 00:10:04 +0800

    no message

--- resources/assets/js/main/components/UserView.vue
+++ resources/assets/js/main/components/UserView.vue
@@ -1,36 +1,42 @@
 <template>
     <div class="user-view-inline">
-        <div class="user-view-info">
-            <UserImg v-if="showimg" class="user-view-img" :info="userInfo" :style="imgStyle"/>
-            <Tooltip v-if="showname" :disabled="loadIng" :delay="delay" :transfer="transfer" :placement="placement" maxWidth="auto" @on-popper-show="getUserData(30)">
-                {{nickname || username}}
-                <div slot="content" style="white-space:normal">
-                    <div>{{$L('用户名')}}: {{username}}</div>
-                    <div>{{$L('职位/职称')}}: {{profession || '-'}}</div>
-                </div>
-            </Tooltip>
-        </div>
+        <Tooltip
+            :disabled="loadIng"
+            :delay="delay"
+            :transfer="transfer"
+            :placement="placement"
+            maxWidth="auto"
+            @on-popper-show="getUserData(30)">
+            <div class="user-view-info">
+                <UserImg v-if="showimg" class="user-view-img" :info="userInfo" :style="imgStyle"/>
+                <div v-if="showname" class="user-view-name">{{nickname || username}}</div>
+            </div>
+            <div slot="content" style="white-space:normal">
+                <div>{{$L('用户名')}}: {{username}}</div>
+                <div>{{$L('职位/职称')}}: {{profession || '-'}}</div>
+            </div>
+        </Tooltip>
     </div>
 </template>
 
 <style lang="scss">
     .user-view-inline {
-        .user-view-info {
-            .ivu-tooltip {
+        .ivu-tooltip {
+            max-width: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            .ivu-tooltip-rel {
                 max-width: 100%;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                .ivu-tooltip-rel {
-                    max-width: 100%;
-                    white-space: nowrap;
-                    overflow: hidden;
-                    text-overflow: ellipsis;
-                }
-            }
-            .user-view-img {
-                .usertext-container-text {
-                    transform: scale(0.86);
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                .user-view-info {
+                    .user-view-img {
+                        .usertext-container-text {
+                            transform: scale(0.86);
+                        }
+                    }
                 }
             }
         }
